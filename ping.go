7b7eb834141637399c5cddbd83dd4173d364809b
Apesosmarc <mapesos@mjels.com>
2023-12-13 14:18:19 -0500

    add exception for 403 status code

--- ping.go
+++ ping.go
@@ -9,23 +9,21 @@ func checkLink(url string, tries int) int {
 	successfulAttempts := 0
 
 	for i := 0; i < tries; i++ {
-		resp, err := http.Get(url)
+		resp, err := http.Get("https://" + url)
 		if err != nil {
 			// Error making the request, the endpoint is considered down
-			fmt.Printf("Attempt %d: Error - %v\n", i+1, err)
+			fmt.Printf("Endpoint: %v Attempt %d: Error - %v\n", url, i+1, err)
 			continue
 		}
 
-		// Check the status code
-		if resp.StatusCode != http.StatusOK {
-			fmt.Printf("Attempt %d: Status Code - %d\n", i+1, resp.StatusCode)
-			// You may customize the condition based on your needs
-			// For example, you might consider non-200 status codes as "down"
+		// 403 = forbidden which means server is responding
+		if resp.StatusCode != http.StatusOK && resp.StatusCode != http.StatusForbidden {
+			fmt.Printf("Endpoint: %v Attempt %d: Status Code - %d\n", url, i+1, resp.StatusCode)
 			continue
 		}
 
 		// The endpoint is up
-		fmt.Printf("Attempt %d: Success\n", i+1)
+		fmt.Printf("Endpoint: %v Attempt %d: Success\n", url, i+1)
 		successfulAttempts++
 	}
 
@@ -34,17 +32,16 @@ func checkLink(url string, tries int) int {
 }
 
 func ping(endpoint string, tries int) (string, error) {
-	url := "https://example.com"
 
-	successfulAttempts := checkLink(url, tries)
+	successfulAttempts := checkLink(endpoint, tries)
 
 	if successfulAttempts == tries {
 		return fmt.Sprintf("%s is up! %d packets transmitted and received", endpoint, tries), nil
 	}
 
-	if successfulAttempts > 0 {
-		return "", fmt.Errorf("the endpoint is experiencing issues! %d successful attempts out of %d tries", successfulAttempts, tries)
-	} else {
-		return "", fmt.Errorf("this endpoint is down after %d attempts", tries)
-	}
+	// handling for if some of the attempts were successful?
+	//return "", fmt.Errorf("the endpoint is experiencing issues! %d successful attempts out of %d tries", successfulAttempts, tries)
+
+	return "", fmt.Errorf("endpoint %v is down after %d attempts", endpoint, tries)
+
 }
